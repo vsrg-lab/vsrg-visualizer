c7f6191f1f289ae301b3e6ef9bbb5de5fb73a69a
CloudHolic <cloudholic@pm.me>
2026-08-14 13:50:17 +0900

    Add boilerplates

---
+++ eslint.config.js
@@ -0,0 +1,55 @@
+import js from "@eslint/js";
+import stylistic from "@stylistic/eslint-plugin";
+import { defineConfig, globalIgnores } from "eslint/config";
+import { importX } from "eslint-plugin-import-x";
+import jsdoc from "eslint-plugin-jsdoc";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite
+    ],
+    languageOptions: {
+      globals: globals.browser
+    },
+    plugins: {
+      "@stylistic": stylistic,
+      "import-x": importX,
+      jsdoc
+    },
+    rules: {
+      "@stylistic/indent": ["error", "tab"],
+      "@stylistic/quotes": ["error", "double"],
+      "@stylistic/jsx-quotes": ["error", "prefer-double"],
+      "@stylistic/semi": ["error", "always"],
+      "@stylistic/comma-dangle": ["error", "never"],
+      "@stylistic/brace-style": ["error", "1tbs", { allowSingleLine: true }],
+      "curly": ["error", "multi"],
+      "@stylistic/nonblock-statement-body-position": ["error", "below"],
+      "@stylistic/arrow-parens": ["error", "as-needed"],
+      "import-x/order": ["error", {
+        groups: ["builtin", "external", "internal", ["parent", "sibling", "index"]],
+        "newlines-between": "always",
+        alphabetize: { order: "asc", caseInsensitive: true }
+      }],
+      "jsdoc/require-jsdoc": ["warn", {
+        publicOnly: true,
+        contexts: ["TSTypeAliasDeclaration", "TSInterfaceDeclaration"],
+        require: {
+          FunctionDeclaration: true,
+          ArrowFunctionExpression: true,
+          ClassDeclaration: true
+        }
+      }]
+    }
+  }
+]);
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+/**
+ *
+ */
+function App() {
+	return <h1>vsrg-visualizer</h1>;
+}
+
+export default App;
